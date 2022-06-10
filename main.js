1a1710f77595b105f4f03127d4281f06fd992480
Jehyun Jung <toojey7@gmail.com>
2022-06-10 15:51:44 +0900

    refactor: changed to express based server
    routing
    get
    post

--- main.js
+++ main.js
@@ -1,171 +1,160 @@
-const http = require('http');
 const fs = require('fs');
-const url = require('url');
-const qs=require("querystring");
 const template=require("./lib/template");
 const path=require('path');
+const express=require('express');
 const sanitizeHtml=require('sanitize-html');
+const qs=require('querystring');
 
-const app = http.createServer(function(request,response){
-    const _url = request.url;
-    const queryData=url.parse(_url,true).query;
-    const pathName=url.parse(_url,true).pathname;
-    let title=queryData.id;
-    //루트 경로로 접근 했을 경우 url의 pathname은 /이다.
-    if(pathName==="/"){
-        //만약 query string이 없는 경우, 이는 메인페이지를 접속한 것을 의미한다.
-        if(title === undefined){
-            fs.readdir("./data",(err,files)=>{
-                const list=template.list(files);
-                title="Welcome";
-                const description="Hello, Node.js";
-                const html=template.html(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
-                response.writeHead(200);
-                response.end(html);
-                })
-            
-            }
-        else{
-            fs.readdir("./data",(err,files)=>{
-                const list=template.list(files);
-                const fileteredId=path.parse(title).base;
-                console.log(err);
-                console.log(path.parse(title));
-                fs.readFile(`./data/${fileteredId}`,'utf8',(err,description)=>{
-                    const sanitizedTitle=sanitizeHtml(title);
-                    const sanitizedDescription=sanitizeHtml(description);
-                    const html=template.html(title,list,
-                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                        `<a href="/create">create</a>
-                        <a href="/update?id=${sanitizedTitle}">update</a>
-                            <form action="/process_delete" method="post">
-                                <input type="hidden" name="id" value="${sanitizedTitle}">
-                                <input type="submit" value="delete"></input>
-                            </form>
-                            `);
-                        response.writeHead(200);
-                        response.end(html);
-                    }
-                );
-                })
-        }
-        
-    }
-    else if(pathName==="/create"){
-        fs.readdir("./data",(err,files)=>{
-            const list=template.list(files);
-            title="WEB - Create";
-            const html=template.html(title,list,`
-                <form action="/process_create" method="post"?
-                    <p>
-                        <input type="text" name="title" placeholder="title">
-                    </p>
-                    <p>
-                        <textarea name="description" placeholder="description"></textarea>
-                    </p>
-                    <p>
-                        <input type="submit">
-                    </p>
-                </form>
-            `,``);
-            response.writeHead(200);
-            response.end(html);
-            });
+//Application 생성
+const app=express();
 
-        }
-    else if(pathName=="/process_create"){
-        let body="";
-        request.on("data",function(data){
-            body+=data;
+
+//Application에 대한 포트 구성
+app.listen(3000,()=>{
+    console.log("App Listening on port 3000");
+})
+
+//root 경로에 대한 라우팅 진행
+app.get("/",(req,res)=>{
+    fs.readdir("./data",(err,files)=>{
+        const list=template.list(files);
+        const title="Welcome Hello";
+        const description="Hello, Node.js";
+        const html=template.html(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
+        res.send(html)
         })
-        request.on("end",function(){
-            const post=qs.parse(body);
-            title=post.title;
-            const description=post.description;
+})
 
-            fs.writeFile(`data/${title}`,description,"utf-8",(err)=>{
-                response.writeHead(302,{
-                    Location:`/?id=${title}`
-                });
-                response.end();
-            })
-        })
-        
-    }
-    else if(pathName=="/update"){
-        fs.readdir("./data",(err,files)=>{
-            const list=template.list(files);
-            const fileteredId=path.parse(title).base;
-            fs.readFile(`./data/${fileteredId}`,'utf8',(err,description)=>{
-                const html=template.html(title,list,
-                    `
-                    <form action="/process_update" method="post">
-                        <input type="hidden" name="id" value="${title}">
-                    <p>
-                        <input type="text" name="title" placeholder="title" value="${title}">
-                    </p>
-                    <p>
-                        <textarea name="description" placeholder="description">${description}</textarea>
-                    </p>
-                    <p>
-                        <input type="submit">
-                    </p>
+//page 경로에 대한 라우팅 진행
+app.get("/page/:pageId",(req,res)=>{
+    fs.readdir("./data",(err,files)=>{
+        const list=template.list(files);
+        const title=req.params.pageId;
+        const fileteredId=path.parse(title).base;
+        console.log(err);
+        console.log(path.parse(title));
+        fs.readFile(`./data/${fileteredId}`,'utf8',(err,description)=>{
+            const sanitizedTitle=sanitizeHtml(title);
+            const sanitizedDescription=sanitizeHtml(description);
+            const html=template.html(title,list,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                `<a href="/create">create</a>
+                <a href="/update/${sanitizedTitle}">update</a>
+                    <form action="/process_delete" method="post">
+                        <input type="hidden" name="id" value="${sanitizedTitle}">
+                        <input type="submit" value="delete"></input>
                     </form>
-                    `,
-                    `<a href="/create">create</a><a href="/update?id=${title}">update</a>`);
-                    response.writeHead(200);
-                    response.end(html);
-                }
-            );
-            })
-    }
-    else if(pathName=="/process_update"){
-        let body="";
-        request.on("data",function(data){
-            body+=data;
-        })
-        request.on("end",function(){
-            const post=qs.parse(body);
-            console.log(post)
-            const id=post.id
-            title=post.title;
-            const description=post.description;
-            const fileteredId=path.parse(title).base;
-            fs.rename(`./data/${id}`,`./data/${title}`,(err)=>{
-                fs.writeFile(`data/${fileteredId}`,description,"utf-8",(err)=>{
-                    response.writeHead(302,{
-                        Location:`/?id=${title}`
-                    });
-                    response.end();
-                }) 
-            });
-            
-        })
-    }
-    else if(pathName === "/process_delete"){
-        let body="";
-        request.on("data",function(data){
-            body+=data;
-        })
-        request.on("end",function(){
-            const post=qs.parse(body);
-            console.log(post)
-            const id=post.id
-            const fileteredId=path.parse(id).base;
-            fs.unlink(`./data/${fileteredId}`,(err)=>{
-                console.log(err)
-                response.writeHead(302,{
-                    Location:`/`
-                });
-                response.end();
-
+                    `);
+                res.send(html);
             }
-            )}
         );
-    }
-    else{
-        response.writeHead(404);
-        response.end("Not Found");
-    }
-});
-app.listen(3000);
+        })
+})
+
+//파일 생성하는 Routing
+app.get("/create",(req,res)=>{
+    fs.readdir("./data",(err,files)=>{
+        const list=template.list(files);
+        const title="WEB - Create";
+        const html=template.html(title,list,`
+            <form action="/process_create" method="post"?
+                <p>
+                    <input type="text" name="title" placeholder="title">
+                </p>
+                <p>
+                    <textarea name="description" placeholder="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+            </form>
+        `,``);
+        res.send(html);
+        });
+})
+
+app.post("/process_create",(req,res)=>{
+    let body="";
+    req.on("data",function(data){
+        body+=data;
+    })
+    req.on("end",function(){
+        const post=qs.parse(body);
+        const title=post.title;
+        const description=post.description;
+
+        fs.writeFile(`data/${title}`,description,"utf-8",(err)=>{
+            res.redirect(`/page/${title}`);
+        })
+    })
+})
+
+//파일 수정에 관한 Routing
+app.get("/update/:pageId",(req,res)=>{
+    fs.readdir("./data",(err,files)=>{
+        const list=template.list(files);
+        const title=req.params.pageId;
+        const fileteredId=path.parse(title).base;
+        fs.readFile(`./data/${fileteredId}`,'utf8',(err,description)=>{
+            const html=template.html(title,list,
+                `
+                <form action="/process_update" method="post">
+                    <input type="hidden" name="id" value="${title}">
+                <p>
+                    <input type="text" name="title" placeholder="title" value="${title}">
+                </p>
+                <p>
+                    <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                    <input type="submit">
+                </p>
+                </form>
+                `,
+                `<a href="/create">create</a><a href="/update/${title}">update</a>`);
+                res.send(html);
+            }
+        );
+        })
+})
+
+
+app.post("/process_update",(req,res)=>{
+    let body="";
+    req.on("data",function(data){
+        body+=data;
+    })
+    req.on("end",function(){
+        const post=qs.parse(body);
+        const id=post.id
+        const title=post.title;
+        const description=post.description;
+        const fileteredId=path.parse(title).base;
+        fs.rename(`./data/${id}`,`./data/${title}`,(err)=>{
+            fs.writeFile(`data/${fileteredId}`,description,"utf-8",(err)=>{
+                res.redirect(`/page/${title}`);
+            }) 
+        });
+        
+    })
+})
+
+//파일 수정에 관한 라우팅
+app.post("/process_delete",(req,res)=>{
+    let body="";
+    req.on("data",function(data){
+        body+=data;
+    })
+    req.on("end",function(){
+        const post=qs.parse(body);
+        const id=post.id
+        const fileteredId=path.parse(id).base;
+        fs.unlink(`./data/${fileteredId}`,(err)=>{
+            console.log(err)
+            res.redirect(`/`);
+
+        }
+        )}
+    );
+})
+
