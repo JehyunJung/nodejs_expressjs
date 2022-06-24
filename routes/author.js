d93796c947dac81756e0b50456aabf379bd412db
Jehyun Jung <toojey7@gmail.com>
2022-06-24 23:35:28 +0900

    feat: added sequelize orm to author routings

--- routes/author.js
+++ routes/author.js
@@ -3,6 +3,7 @@ const template=require('../lib/template.js');
 const express=require('express');
 const router=express.Router();
 const mysql_connection=require('../lib/mysql.js');
+const { Author } = require('../models/index.js');
 
 router.get("/",(req,res)=>{
     
@@ -23,7 +24,7 @@ router.get("/",(req,res)=>{
 
 //파일 생성하는 Routing
 router.get("/create",(req,res)=>{
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect("/author");
         return false;
     }
@@ -52,7 +53,7 @@ router.get("/create",(req,res)=>{
 });
 
 router.post("/create_process",(req,res)=>{
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect("/author");
         return false;
     }
@@ -61,64 +62,62 @@ router.post("/create_process",(req,res)=>{
             name,profile
         }
     }=req;
-    mysql_connection.query(
-        "INSERT INTO AUTHOR(name,profile) VALUES(?,?)"
-        ,[name,profile]
-        ,(err,results)=>{
-            if(err){
-                throw err;
-            }
-            res.redirect(`/author`);
-        })
+    Author.create({
+        name:name,
+        profile:profile
+    }).then(()=>{
+        res.redirect(`/author`);
+    }).catch((err)=>{
+        throw err;
+    });
+
 })
 
 //파일 수정에 관한 Routing
 router.get("/update/:authorId",(req,res)=>{
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect("/author");
         return false;
     }
     const topic_list=req.topic_list;
     const id=req.params.authorId;
     const title = 'Welcome';
-    mysql_connection.query(
-        "SELECT * FROM AUTHOR WHERE ID=?",
-        [id],
-        (err,author)=>{
-            if(err){
-                throw err;
-            }
-            const name=author[0].name;
-            const profile=author[0].profile;
-            const html=template.html(title,topic_list,
-            `
-            ${template.author_list(req.authors)}
-            <form action="/author/update_process" method="post">
-                <input type="hidden" name="id" value="${id}">
-                <p>
-                    <input name="author" placeholder="name" value="${name}">
-                </p>
-                <p>
-                    <textarea name="profile" placeholder="profile">${profile}</textarea>
-                </p>
-                <p>
-                    <input type="submit" value="update">
-                </p>
-            </form>
-            `,
-            ``,
-            template.auth_loginButton(req,res),
-            template.auth_loginForm(false),        
-            template.auth_joinButton(req,res),
-            template.auth_joinForm(false));
-            res.send(html);
-        })
-        
-    });
-
+    Author.findOne({
+        where:{
+            id:id
+        }
+    }).then((author)=>{
+        const name=author.name;
+        const profile=author.profile;
+        const html=template.html(title,topic_list,
+        `
+        ${template.author_list(req.authors)}
+        <form action="/author/update_process" method="post">
+            <input type="hidden" name="id" value="${id}">
+            <p>
+                <input name="author" placeholder="name" value="${name}">
+            </p>
+            <p>
+                <textarea name="profile" placeholder="profile">${profile}</textarea>
+            </p>
+            <p>
+                <input type="submit" value="update">
+            </p>
+        </form>
+        `,
+        ``,
+        template.auth_loginButton(req,res),
+        template.auth_loginForm(false),        
+        template.auth_joinButton(req,res),
+        template.auth_joinForm(false));
+        res.send(html);
+    }).catch((err)=>{
+        throw err;
+    })
+})
 
 router.post("/update_process",(req,res)=>{
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect("/author");
         return false;
     }
@@ -127,36 +126,40 @@ router.post("/update_process",(req,res)=>{
             id,author,profile
         }
     }=req;
-    mysql_connection.query(
-        "UPDATE AUTHOR SET NAME=?,PROFILE=? WHERE ID=?"
-        ,[author,profile,id],
-        (err,results)=>{
-            if(err){
-                throw err;
-            }
-            res.redirect(`/author`);
-        });
+    Author.update({
+        name:author,
+        profile:profile
+    },
+    {
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect(`/author`);
+    }).catch((err)=>{
+        throw err;
+    });
 
 })
 
 //파일 제거에 관한 라우팅
 router.post("/delete_process",(req,res)=>{
-    //console.log("Deleting: " + req.session.is_logined);
-    if(!req.session.is_logined){
+    //console.log("Deleting: " + req.user);
+    if(!req.user){
         res.redirect("/author");
         return false;
     }
     const id=req.body.id;
-    mysql_connection.query(
-        "DELETE FROM AUTHOR WHERE ID=?"
-        ,[id]
-        ,(err,results)=>{
-            if(err){
-                throw err;
-            }
-            res.redirect("/author");
+    Author.destroy({
+        where:{
+            id:id
         }
-    )
+    }).then(()=>{
+        res.redirect("/author");
+    }).catch((err)=>{
+        throw err;
+    });
+
 });
 
 module.exports=router;
