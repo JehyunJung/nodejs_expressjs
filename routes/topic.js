90f80a6563d3e5e71e15ab2be80bab6cf89ba2d6
Jehyun Jung <toojey7@gmail.com>
2022-06-24 23:35:33 +0900

    feat: added sequelize orm to topicroutings

--- routes/topic.js
+++ routes/topic.js
@@ -4,11 +4,12 @@ const template=require('../lib/template.js');
 const express=require('express');
 const router=express.Router();
 const mysql_connection=require('../lib/mysql.js');
+const {sequelize,User,Author,Topic}= require("../models");
 
 
 //파일 생성하는 Routing
 router.get("/create",(req,res)=>{
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect("/");
         return false;
     }
@@ -42,39 +43,39 @@ router.post("/create_process",(req,res)=>{
             title,author,description
         }
     }=req;
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect(`/`);
         return false;
     }
-    mysql_connection.query(
-        "INSERT INTO TOPIC(title,description,created,author_id) VALUES(?,?,now(),?)"
-        ,[title,description,author]
-        ,(err,results)=>{
-            if(err){
-                throw err;
-            }
-            res.redirect(`/topic/${results.insertId}`);
-        })
+    Topic.create({
+        title:title,
+        description:description,
+        author_id:author
+    }).then((result)=>{
+        res.redirect(`/topic/${result.id}`);
+    }).catch((err)=>{
+        throw err;
+    });
 })
 
 //파일 수정에 관한 Routing
 router.get("/update/:topicId",(req,res)=>{
     const topic_list=req.topic_list;
     const id=req.params.topicId;
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect(`/topic/${id}`);
         return false;
     }
-    mysql_connection.query(
-        "SELECT * FROM TOPIC WHERE ID=?",
-        [id],
-        (err,topic)=>{
-            if(err){
-                next(err);
-            }
-            const title=topic[0].title;
-            const description=topic[0].description;
-            const author_id=topic[0].author_id;
+    Topic.findOne({
+        where:{id:id},
+        include:[{
+            model:Author,
+            required:true
+        }]
+    }).then((topic)=>{
+            const title=topic.title;
+            const description=topic.description;
+            const author_id=topic.author_id;
             const html=template.html(title,topic_list,
             `
             <form action="/topic/update_process" method="post">
@@ -97,93 +98,97 @@ router.get("/update/:topicId",(req,res)=>{
             template.auth_joinButton(req,res),
             template.auth_joinForm(false));
             res.send(html);
-        })
-        
+    }).catch((err)=>{
+        throw err;
     });
+        
+});
 
 
 router.post("/update_process",(req,res)=>{
-
     const{
         body:{
             id,title,author,description
         }
     }=req;
 
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect(`/topic/${id}`);
         return false;
     }
-    mysql_connection.query(
-        "UPDATE TOPIC SET TITLE=?,DESCRIPTION=?,AUTHOR_ID=? WHERE ID=?"
-        ,[title,description,author,id],
-        (err,results)=>{
-            if(err){
-                throw err;
-            }
-            res.redirect(`/topic/${id}`);
-        });
+    Topic.update({
+        title:title,
+        description:description,
+        author_id:author
+    },
+    {
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect(`/topic/${id}`);
+    }).catch((err)=>{
+        throw err;
+    });
 
 })
 
 //파일 제거에 관한 라우팅
 router.post("/delete_process",(req,res)=>{
     const id=req.body.id;
-    if(!req.session.is_logined){
+    if(!req.user){
         res.redirect(`/topic/${id}`);
         return false;
     }
-    mysql_connection.query(
-        "DELETE FROM TOPIC WHERE ID=?"
-        ,[id]
-        ,(err)=>{
-            if(err){
-                throw err;
-            }
-            res.redirect("/");
+    Topic.destroy({
+        where:{
+            id:id
         }
-    )
+    }).then(()=>{
+        res.redirect("/");
+    }).catch((err)=>{
+        throw err;
+    });
 });
 //topic 경로에 대한 라우팅 진행
-router.get("/:topicId",(req,res,next)=>{
+router.get("/:topicId",async (req,res,next)=>{
     const topic_list=req.topic_list;
     const id=req.params.topicId;
-    mysql_connection.query(
-        `SELECT topic.id,title,description,name,profile From topic LEFT JOIN author on topic.author_id=author.id where topic.id=?`,
-        [id],
-        (err,topic)=>{
-            if(err){
-                next(err);
-            }
-            else{
-                console.log(topic);
-                const title=topic[0].title;
-                const description=topic[0].description;
-                
-                const author_name=topic[0].name;
-                const author_profile=topic[0].profile;
-                const sanitizedTitle=sanitizeHtml(title);
-                const sanitizedDescription=sanitizeHtml(description);
-                const html=template.html(title,topic_list,
-                    `
-                    <h2>Title: ${sanitizedTitle}</h2>
-                    <h3> Author: ${author_name}</h3>
-                    <h3> Profile: ${author_profile}</h3>
-                    <p> Description: ${sanitizedDescription}</p>`,
-                    `<a href="/topic/create">create</a>
-                    <a href="/topic/update/${id}">update</a>
-                        <form action="/topic/delete_process" method="post">
-                            <input type="hidden" name="id" value="${id}">
-                            <input type="submit" value="delete"></input>
-                        </form>
-                        `,
-                        template.auth_loginButton(req,res),
-                        template.auth_loginForm(false),        
-                        template.auth_joinButton(req,res),
-                        template.auth_joinForm(false));
-                    res.send(html);
-            }
-        })
-    });
+    Topic.findOne({
+        where:{id:id},
+        include:[{
+            model:Author,
+            required:true
+        }]
+    }).then((topic)=>{
+        const title=topic.title;
+        const description=topic.description;
+        
+        const author_name=topic.Author.name;
+        const author_profile=topic.Author.profile;
+        const sanitizedTitle=sanitizeHtml(title);
+        const sanitizedDescription=sanitizeHtml(description);
+        const html=template.html(title,topic_list,
+            `
+            <h2>Title: ${sanitizedTitle}</h2>
+            <h3> Author: ${author_name}</h3>
+            <h3> Profile: ${author_profile}</h3>
+            <p> Description: ${sanitizedDescription}</p>`,
+            `<a href="/topic/create">create</a>
+            <a href="/topic/update/${id}">update</a>
+                <form action="/topic/delete_process" method="post">
+                    <input type="hidden" name="id" value="${id}">
+                    <input type="submit" value="delete"></input>
+                </form>
+                `,
+                template.auth_loginButton(req,res),
+                template.auth_loginForm(false),        
+                template.auth_joinButton(req,res),
+                template.auth_joinForm(false));
+            res.send(html);
+    }).catch((error)=>{
+        throw error;
+    })
+});
 
 module.exports=router;
